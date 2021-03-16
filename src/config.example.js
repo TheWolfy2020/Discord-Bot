7d941e0b28f0248e9d2c9848e95d32a33370d621
The Wolfy 2020 <77306871+TheWolfy2020@users.noreply.github.com>
2021-03-15 22:16:32 -0400

    Update config.example.js

--- src/config.example.js
+++ src/config.example.js
@@ -1,24 +1,21 @@
 const config = {
-	ownerID: ['YourAccountID'],
-	token: 'YourBotToken',
-	botClient: 'YourBotClientSecret',
+	ownerID: ['522965553472864256'],
+	token: 'Nzk4Mjk0NDIyMDU1Mjg4ODYy.X_y7nQ.s0ju4Qf4tFQ7o0QfhEK5n1lUxqo',
+	botClient: '798294422055288862',
 	// For looking up Twitch, Fortnite, Steam accounts
 	api_keys: {
 		// https://dev.twitch.tv/console/apps
-		twitch: {
-			clientID: '',
-			clientSecret: '',
-		},
+		twitch: 'wxwsd54atuqlv3ep13qj4b0wietaqn',
 		// https://fortnitetracker.com/site-api
 		fortnite: 'fortniteAPI-Key',
 		// https://api.ksoft.si/
-		ksoft: 'ksoftAPI-Key',
+		ksoft: 'eyJ0IjogImFwcCIsICJrIjogImtobWFjc2lnIiwgInBrIjogNTg0OCwgIm8iOiAiNTIyOTY1NTUzNDcyODY0MjU2IiwgImMiOiAxNzY2NTIyN30.7dbff6bc26b23967d5061d03ce33a776ebfa3d97bb24c8cf4649181683499fdf',
 		// https://steamcommunity.com/dev
 		steam: 'steamAPI-Key',
 		// https://developer.spotify.com/documentation/web-api/
 		spotify: {
-			iD: 'spotify-ID',
-			secret: 'spotify-secret',
+			iD: '921b7500d86d48b0bf61890be04fcfa9',
+			secret: '5cbe8aec626d4f978e28be042a225b3b',
 		},
 		// Your Ubisoft email and password (You don't need to enable anything)
 		rainbow: {
@@ -26,11 +23,11 @@ const config = {
 			password: 'password',
 		},
 		// https://genius.com/developers
-		genuis: 'genuisAPI-KEY',
+		genuis: 'gvrYMVGp9hA09_uX1fJF40NS_8Rc1wBkLO5HvfRvcB8BmwkjVObLN0NL3jW8GIG1',
 		// https://amethyste.moe/en
 		amethyste: 'amethysteAPI-Key',
 	},
-	// add plugins/commands here if you don't want them loaded in the bot.
+	// IF you want any commands/plugins disabled
 	disabledCommands: [],
 	disabledPlugins: [],
 	// This is a list of websites that the bot is on. API to interect with server/shard count
@@ -46,15 +43,15 @@ const config = {
 	// your support server
 	SupportServer: {
 		// Link to your support server
-		link: 'https://discord.gg/8g6zUQu',
+		link: 'https://discord.gg/2fQJfUTVSg',
 		// Your support's server ID
-		GuildID: '750822670505082971',
+		GuildID: '721654305358086215',
 		// This for using the suggestion command on your server
-		ModRole: '751857618720522341',
+		ModRole: '805938537614147584',
 		// What channel to post the suggestions
-		SuggestionChannel: '761619652009787392',
+		SuggestionChannel: '721791372637765642',
 		// Where the bot will send Guild join/leave messages to
-		GuildChannel: '761619652009787392',
+		GuildChannel: '721654305358086219',
 	},
 	// This is just so some commands can be ran in DM channels
 	defaultSettings: {
@@ -68,7 +65,7 @@ const config = {
 		tick: ':white_check_mark:',
 	},
 	// URL to mongodb
-	MongoDBURl: 'mongodb://link',
+	MongoDBURl: 'mongodb+srv://bot:5540@bot.3482r.mongodb.net/Bot?retryWrites=true&w=majority',
 	// if you want debugging turned on or not
 	debug: false,
 };
